d55e06ed01ed54021bab897414dd98899d92038d
sanjaya-cy <141553741+sanjaya-cy@users.noreply.github.com>
2026-02-15 02:55:19 +0000

    add comments

--- aplikasi-login/assets/scripts/login-script.js
+++ aplikasi-login/assets/scripts/login-script.js
@@ -5,27 +5,29 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+// create variable each view's elements
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+// create variable to save email and password
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+// add click action to the button
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+    // get value from each input (email and password) when button clicked
     const email = inputEmailElement.value;
     const password = inputPasswordElement.value;
   
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
+    // check if email and paasword are equal with database
     if (email == expectedEmail && password == expectedPassword){
+      // go to home page
       goToHome();
     } else {
+      //error pop up wrong input
       showPopup;
     }
 });
